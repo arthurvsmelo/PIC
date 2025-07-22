00c22143cb6e8953c944c03c4555f390389386f9
Arthur Melo <arthurmelo775@gmail.com>
2025-07-22 09:48:56 -0300

    commit inicial do projeto

---
+++ projetos/relogio.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Arthur
+ *
+ * Created on July 22, 2025, 9:45 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
